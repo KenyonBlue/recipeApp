7e366dfaf4598ce1f8a98ade7a3c1758ce000990
Kenyon Blue <bluekenyfleo@gmail.com>
2017-07-17 07:27:14 -0400

    added the recipies to the recipe page - next step is adding the details into the button

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { EditRecipePage } from "../editrecipe/editrecipe";
+import { Recipe } from "../../models/recipe";
+import { RecipesService } from "../../services/recipes";
+
 
 @Component({
   selector: 'page-recipes',
   templateUrl: 'recipes.html'
 })
 export class RecipesPage {
-
-  constructor(public navCtrl: NavController) {
-
+     recipes: Recipe[];
+     
+     
+  constructor(public navCtrl: NavController, private recipesService: RecipesService) {
   }
+     ionViewWillEnter(){
+          this.recipes = this.recipesService.getRecipes();
+     }
      
      onNewRecipe(){
           this.navCtrl.push(EditRecipePage, {mode: 'New'});
+     }
+     
+     onLoadRecipe(){
+          
      }
 
 }
